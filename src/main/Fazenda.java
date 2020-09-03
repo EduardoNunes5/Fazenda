c58ccb0eaefcbc73a205b9ff0b56c0b68d82fd21
Eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-09-03 17:20:04 -0300

    input morcego implementado

--- src/main/Fazenda.java
+++ src/main/Fazenda.java
@@ -1,9 +1,38 @@
 package main;
 
+import entities.Pato;
+
 import java.util.Scanner;
 
 public class Fazenda {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        menu();
+        int option = sc.nextInt();
+
+        Pato pato = null;
+        switch(option){
+            case 1:
+                System.out.print("Digite a idade do animal:  ");
+                int age = sc.nextInt();
+                sc.nextLine();
+                System.out.print("Digite o tamanho do animal:  ");
+                String size = sc.nextLine();
+                System.out.print("Digite a cor da pena pato:  ");
+                String color = sc.nextLine();
+                pato = new Pato(age, size, color);
+                break;
+        }
+
+        System.out.println(pato);
+    }
+
+    public static void menu(){
+        System.out.println("Digite 1 para criar um pato.");
+        System.out.println("Digite 2 para criar um morcego");
+        System.out.println();
+        System.out.println();
+        System.out.print(">> ");
     }
 }
